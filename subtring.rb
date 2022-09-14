adc371673f6107659be0a829f139b09961c354bb
Aliyu <biggy0023@gmail.com>
2022-09-14 21:21:57 +0100

    made the code cleaner

--- subtring.rb
+++ subtring.rb
@@ -1,28 +1,15 @@
-dictionary = [
-  "below",
-  "down",
-  "go",
-  "going",
-  "horn",
-  "how",
-  "howdy",
-  "it",
-  "i",
-  "low",
-  "own",
-  "part",
-  "partner",
-  "sit"
-]
+# frozen_string_literal: false
 
-def substring(string, dictionary)
-  string = string.downcase #convert all inputs to downcase
-  result = Hash.new(0) #creates new hash the result will be displayed in
-  dictionary.each do |input| #iterating through the dictionary
-    match = string.scan(input).length #finds out if the string matches words in the dictionary
-    result[input] = match unless match == 0 #print all forms of matches unless it's zero
+dictionary = %w[below down go going horn how howdy it i low own part partner sit]
+
+def substrings(argument, dictionary)
+  str = argument.downcase
+  result = Hash.new(0)
+  dictionary.each do |word|
+    match = str.scan(word).length
+    result[word] = match unless match.zero?
   end
-  result #returns theh hash
+  result
 end
 
-substring("Go down below", dictionary)
+substrings('hellow world hello ali', dictionary)
